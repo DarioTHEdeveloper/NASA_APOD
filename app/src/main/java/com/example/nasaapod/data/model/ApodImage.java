59b12e359a36d5d1f6df4d8220b84e64c1889fcf
Tobias Pfatrisch <tobias.pfatrisch@adac.de>
2022-05-04 13:40:38 +0200

    ApodImage Model: TODO instruction

---
+++ app/src/main/java/com/example/nasaapod/data/model/ApodImage.java
@@ -0,0 +1,11 @@
+package com.example.nasaapod.data.model;
+
+/**
+ * TODO: Fill model with variables like in PlaceholderItem.java returned by https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY
+ *
+ */
+
+public class ApodImage {
+
+
+}
