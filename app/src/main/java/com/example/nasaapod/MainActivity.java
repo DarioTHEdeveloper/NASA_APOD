8732a247d73b6c64f457710684b2e8127d22b899
Tobias Pfatrisch <tobias.pfatrisch@adac.de>
2022-05-04 18:10:16 +0200

    closes #8; API call + tutorial

--- app/src/main/java/com/example/nasaapod/MainActivity.java
+++ app/src/main/java/com/example/nasaapod/MainActivity.java
@@ -2,10 +2,20 @@ package com.example.nasaapod;
 
 import android.os.Bundle;
 
+import com.example.nasaapod.data.ApodService;
+import com.example.nasaapod.data.model.ApodImage;
 import com.example.nasaapod.databinding.CardRecyclerBinding;
-import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
-import android.view.View;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,12 +28,67 @@ public class MainActivity extends AppCompatActivity {
         binding = CardRecyclerBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-//        binding.fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
+
+        /**
+         * TODO: add recyclerView binding
+         */
+        RecyclerView cardRecycler = null;
+
+
+        int spanCount = 2;
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, spanCount);
+        /**
+         * TODO: call setLayoutManager method on cardRecycler to set gridLayoutManager from above
+         */
+
+        /**
+         * TODO: create ApodAdapter with context parameter
+         */
+        ApodRecyclerAdapter apodAdapter = null;
+
+        /**
+         * TODO: call setAdapter method on cardRecycler to set apodAdapter
+         */
+
+        /**
+         * TODO: replace null with method call
+         */
+        Call<List<ApodImage>> imagesCall = null;
+
+        imagesCall.enqueue(new Callback<List<ApodImage>>() {
+            @Override
+            public void onResponse(Call<List<ApodImage>> call, Response<List<ApodImage>> response) {
+
+                List<ApodImage> apodImageList = response.body();
+                /*
+                TODO: call setImages method on adapter with list returned from response body
+                 */
+
+                /*
+                TODO: call notifyDataSetChanged to update recyclerview
+                 */
+
+            }
+            @Override
+            public void onFailure(Call<List<ApodImage>> call, Throwable t) {}
+        });
+    }
+
+    private Call<List<ApodImage>> getImageListFromAPI() {
+
+        String apiKey = "DEMO_KEY";
+        int APOD_COUNT = 30;
+
+        Retrofit retrofit = new Retrofit
+                .Builder()
+                /* add Gson */
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl("https://api.nasa.gov/planetary/")
+                .build();
+
+        ApodService apodService = retrofit.create(ApodService.class);
+        Call<List<ApodImage>> call = apodService.apod(apiKey, APOD_COUNT);
+
+        return call;
     }
 }
